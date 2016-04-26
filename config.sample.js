6ce50375e0e2be8b3eeb878b72ea6b9d12ff174f
Neson <neson@dex.tw>
2016-04-26 09:31:24 +0800

    setup travis

--- config.sample.js
+++ config.sample.js
@@ -3,7 +3,7 @@ module.exports = {
   googleAnalyticsID: '',
   androidPackageName: 'io.expenture.expense',
   androidReleaseKeystoreFile: 'release-key.keystore',
-  androidReleaseKeystorePassword: '',
-  androidReleaseKeyAlias: '',
-  androidReleaseKeyPassword: ''
+  androidReleaseKeystorePassword: 'keystore_password_here',
+  androidReleaseKeyAlias: 'key_alias_here',
+  androidReleaseKeyPassword: 'key_password_here'
 };
